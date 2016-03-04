a466663255978e8b4a2fb1a4aeefb304e5e5d744
xxy-201251076 <731109251@qq.com>
2016-03-04 10:07:00 +0800

    init

---
+++ ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  MyItem
+//
+//  Created by scsys on 16/3/4.
+//  Copyright © 2016年 于晓晓. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //1.0已经完成
+    
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
